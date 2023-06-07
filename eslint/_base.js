5f0dc45e6a6bdd2197681464e9eb1e8346a39902
Bo Lingen <bo@lingen.life>
2023-06-07 07:39:10 -0500

    build(eslint): add missing plugin

--- eslint/_base.js
+++ eslint/_base.js
@@ -21,6 +21,7 @@ module.exports = {
 		require.resolve("./rules/unicorn"),
 		require.resolve("./rules/variables")
 	],
+	plugins: ["simple-import-sort"],
 	env: {
 		[`es${ecmaVersion}`]: true
 	},
