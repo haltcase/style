c5f4e5a5e0c0f9943160e8db0107f1ab20f4857b
haltcase <bo@haltcase.dev>
2023-06-11 16:05:29 -0500

    ci: add workflow for releasing via PR comment

---
+++ .github/scripts/release-via-comment.js
@@ -0,0 +1,49 @@
+const baseRef = "main";
+const headRef = "canary";
+const workflowToTrigger = "release.yml";
+
+module.exports = async ({ github, context }) => {
+	console.log(
+		`Merging commits for release from ${context.repo.owner}/${context.repo.repo}#${context.issue.number}`
+	);
+
+	const scope = {
+		owner: context.repo.owner,
+		repo: context.repo.repo
+	};
+
+	const { data: pr } = await github.pulls.get({
+		...scope,
+		pull_number: context.issue.number
+	});
+
+	if (pr.head.ref !== headRef || pr.base.ref !== baseRef) {
+		await github.issues.createComment({
+			...scope,
+			issue_number: context.issue.number,
+			body: `Releases can only be triggered when merging \`${headRef}\` → \`${baseRef}\``
+		});
+
+		process.exit(1);
+	}
+
+	if (!pr.mergeable || !pr.merge_commit_sha) {
+		await github.issues.createComment({
+			...scope,
+			issue_number: context.issue.number,
+			body: "This PR cannot be merged. Please fix any conflicts and try again."
+		});
+
+		process.exit(1);
+	}
+
+	await github.rest.actions.createWorkflowDispatch({
+		owner: context.repo.owner,
+		repo: context.repo.name,
+		workflow_id: workflowToTrigger,
+		ref: headRef,
+		inputs: {
+			acknowledge: true
+		}
+	});
+};
